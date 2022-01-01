03260e47c1f1228387aa46ab6687b8c0953a5582
Andrew Gillham <gillham@roadsign.com>
2022-01-01 00:03:14 -0600

    Include the correct dtb file for SOQuartz.

--- build.sh
+++ build.sh
@@ -60,7 +60,7 @@ build_fit() {
 	echo " => Building FIT (${tag})"
 	./scripts/extractbl31.py rkbin/${BL31}
 	cp -f Build/${board}/${RKUEFIBUILDTYPE}_GCC5/FV/RK356X_EFI.fd Build/RK356X_EFI.fd
-	./rkbin/tools/mkimage -f uefi.its -E ${board}_EFI_${tag}.itb
+	./rkbin/tools/mkimage -f uefi_${board}.its -E ${board}_EFI_${tag}.itb
 	rm -f bl31_0x*.bin Build/RK356X_EFI.fd
 }
 
