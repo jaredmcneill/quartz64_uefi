eacf36fbee6545dd160b38da2a248b0f65f63bd5
S199p.Wa1k9r <s199p.wa1k9r@gmail.com>
2022-01-18 08:53:21 +0000

    Adding SD card imaging rules for RK3568 Board

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ shift
 export WORKSPACE="$PWD"
 export PACKAGES_PATH=$PWD/edk2:$PWD/edk2-platforms:$PWD/edk2-non-osi:$PWD/edk2-rockchip
 export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
+export GCC5_AARCH64_PREFIX=aarch64-unknown-linux-gnu-
 
 fetch_deps() {
 	git submodule update --init --recursive
@@ -30,9 +31,11 @@ build_uefi() {
 }
 
 build_idblock() {
-	echo " => Building idblock.bin"
+	SOC=$1
+	DDR=$(grep '^Path1=.*_ddr_' rkbin/RKBOOT/${SOC}MINIALL.ini | cut -d = -f 2-)
+	echo " => Building idblock.bin for ${SOC}"
 	FLASHFILES="FlashHead.bin FlashData.bin FlashBoot.bin"
-	rm -f idblock.bin rk3566_ddr_*.bin rk356x_usbplug*.bin UsbHead.bin ${FLASHFILES}
+	rm -f idblock_${SOC}.bin rk356*_ddr_*.bin rk356x_usbplug*.bin UsbHead.bin ${FLASHFILES}
 
 	# Default DDR image uses 1.5M baud. Patch it to use 115200 to match UEFI first.
 	cat rkbin/tools/ddrbin_param.txt | sed 's/^uart baudrate=/&115200/' > ddrbin_param.txt
@@ -40,14 +43,14 @@ build_idblock() {
 	rm -f ddrbin_param.txt
 
 	# Create idblock.bin
-	(cd rkbin && ./tools/boot_merger RKBOOT/RK3566MINIALL.ini)
+	(cd rkbin && ./tools/boot_merger RKBOOT/${SOC}MINIALL.ini)
 	./rkbin/tools/boot_merger unpack --loader rkbin/rk356x_spl_loader_*.bin --output .
-	cat ${FLASHFILES} > idblock.bin
+	cat ${FLASHFILES} > idblock_${SOC}.bin
 	(cd rkbin && git checkout ${DDR})
 
 	# Cleanup
 	rm -f rkbin/rk356x_spl_loader_*.bin
-	rm -f rk3566_ddr_*.bin rk356x_usbplug*.bin UsbHead.bin ${FLASHFILES}
+	rm -f rk356*_ddr_*.bin rk356x_usbplug*.bin UsbHead.bin ${FLASHFILES}
 }
 
 build_fit() {
@@ -63,25 +66,32 @@ build_fit() {
 fetch_deps
 
 BL31=$(grep '^PATH=.*_bl31_' rkbin/RKTRUST/RK3568TRUST.ini | cut -d = -f 2-)
-DDR=$(grep '^Path1=.*_ddr_' rkbin/RKBOOT/RK3566MINIALL.ini | cut -d = -f 2-)
 
 test -r rkbin/${BL31} || (echo "rkbin/${BL31} not found"; false)
 . edk2/edksetup.sh
 
 build_uefitools
 
+# RK3566 boards
 # Quartz64 boards
 build_uefi Pine64 Quartz64
 build_fit Quartz64
 # SOQuartz modules
 build_uefi Pine64 SOQuartz
 build_fit SOQuartz
-# ROC-RK356x-PC boards
+# ROC-RK3566-PC board
 build_uefi Firefly ROC-RK3566-PC
 build_fit ROC-RK3566-PC
 
-build_idblock
+build_idblock RK3566
+
+# RK3568 boards
+# ROC-RK3568-PC board
+build_uefi Firefly ROC-RK3568-PC
+build_fit ROC-RK3568-PC
+
+build_idblock RK3568
 
 echo
-ls -l `pwd`/idblock.bin `pwd`/*_EFI.itb
+ls -l `pwd`/idblock_*.bin `pwd`/*_EFI.itb
 echo
