4a8b4ad9596ed0f5413253c28d9fdd48550af52f
Jared McNeill <jmcneill@invisible.ca>
2023-03-12 09:52:11 -0300

    Quartz64: Initialize PMIC.

--- edk2-rockchip/Platform/Pine64/Quartz64/Drivers/BoardInitDxe/BoardInitDxe.c
+++ edk2-rockchip/Platform/Pine64/Quartz64/Drivers/BoardInitDxe/BoardInitDxe.c
@@ -64,6 +64,17 @@
 
 #define PMIC_CHIP_NAME          0xed
 #define PMIC_CHIP_VER           0xee
+#define PMIC_POWER_EN1          0xb2
+#define PMIC_POWER_EN2          0xb3
+#define PMIC_POWER_EN3          0xb4
+#define PMIC_LDO1_ON_VSEL       0xcc
+#define PMIC_LDO2_ON_VSEL       0xce
+#define PMIC_LDO3_ON_VSEL       0xd0
+#define PMIC_LDO4_ON_VSEL       0xd2
+#define PMIC_LDO6_ON_VSEL       0xd6
+#define PMIC_LDO7_ON_VSEL       0xd8
+#define PMIC_LDO8_ON_VSEL       0xda
+#define PMIC_LDO9_ON_VSEL       0xdc
 
 /*
  * CPU_GRF registers
@@ -298,6 +309,18 @@ PmicRead (
 }
 
 STATIC
+EFI_STATUS
+PmicWrite (
+  IN UINT8 Register,
+  IN UINT8 Value
+  )
+{
+  return I2cWrite (I2C0_BASE, PMIC_I2C_ADDR,
+                  &Register, sizeof (Register),
+                  &Value, sizeof (Value));
+}
+
+STATIC
 VOID
 BoardInitPmic (
   VOID
@@ -334,6 +357,21 @@ BoardInitPmic (
 
   DEBUG ((DEBUG_INFO, "PMIC: Detected RK%03X ver 0x%X\n", ChipName, ChipVer));
   ASSERT (ChipName == 0x817);
+
+  /* Initialize PMIC */
+  PmicWrite (PMIC_LDO1_ON_VSEL, 0x30);  /* 1.8V - vcca1v8_pmu */
+  PmicWrite (PMIC_LDO2_ON_VSEL, 0x0c);  /* 0.9V - vdda_0v9 */
+  PmicWrite (PMIC_LDO3_ON_VSEL, 0x0c);  /* 0.9V - vdd0v9_pmu */
+  PmicWrite (PMIC_LDO4_ON_VSEL, 0x6c);  /* 3.3V - vccio_acodec */
+  /* Skip LDO5 for now; 1.8V/3.3V - vccio_sd */
+  PmicWrite (PMIC_LDO6_ON_VSEL, 0x6c);  /* 3.3V - vcc3v3_pmu */
+  PmicWrite (PMIC_LDO7_ON_VSEL, 0x30);  /* 1.8V - vcca_1v8 */
+  PmicWrite (PMIC_LDO8_ON_VSEL, 0x30);  /* 1.8V - vcca1v8_dvp */
+  PmicWrite (PMIC_LDO9_ON_VSEL, 0x58);  /* 2.8V - vcca2v8_dvp */
+
+  PmicWrite (PMIC_POWER_EN1, 0xff); /* LDO1, LDO2, LDO3, LDO4 */
+  PmicWrite (PMIC_POWER_EN2, 0xee); /* LDO6, LDO7, LDO8 */
+  PmicWrite (PMIC_POWER_EN3, 0x33); /* LDO9, BOOST */
 }
 
 EFI_STATUS
@@ -345,12 +383,12 @@ BoardInitDriverEntryPoint (
 {
   DEBUG ((DEBUG_INFO, "BOARD: BoardInitDriverEntryPoint() called\n"));
 
-  SocSetDomainVoltage (PMUIO2, VCC_3V3);
-  SocSetDomainVoltage (VCCIO1, VCC_3V3);
-  SocSetDomainVoltage (VCCIO2, VCC_1V8);
-  SocSetDomainVoltage (VCCIO3, VCC_3V3);
+  SocSetDomainVoltage (PMUIO2, VCC_3V3);  /* VCC3V3_PMU */
+  SocSetDomainVoltage (VCCIO1, VCC_3V3);  /* VCCIO_ACODEC (PCIe) */
+  SocSetDomainVoltage (VCCIO2, VCC_1V8);  /* VCCIO_FLASH */
+  SocSetDomainVoltage (VCCIO3, VCC_3V3);  /* VCCIO_SD */
   SocSetDomainVoltage (VCCIO4, VCC_1V8);
-  SocSetDomainVoltage (VCCIO5, VCC_1V8);
+  SocSetDomainVoltage (VCCIO5, VCC_3V3);
   SocSetDomainVoltage (VCCIO6, VCC_1V8);
   SocSetDomainVoltage (VCCIO7, VCC_3V3);
 
