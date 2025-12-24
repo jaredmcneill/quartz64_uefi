46195f76fef3eeaa0007f326e88278a7633c6d8b
Jeff Frasca <thatguy@jeff-frasca.name>
2025-12-24 22:28:21 +0000

    First pass at PHY init

---
+++ edk2-rockchip/Platform/HardKernel/Odroid-M1S/Drivers/BoardInitDxe/EthernetPhy.h
@@ -0,0 +1,17 @@
+/** @file
+ *
+ *  Copyright (c) 2022, Jared McNeill <jmcneill@invisible.ca>
+ *
+ *  SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ **/
+
+#ifndef ETHERNETPHY_H__
+#define ETHERNETPHY_H__
+
+VOID
+EthernetPhyInit (
+    IN EFI_PHYSICAL_ADDRESS GmacBase
+    );
+
+#endif /* ETHERNETPHY_H__ */
